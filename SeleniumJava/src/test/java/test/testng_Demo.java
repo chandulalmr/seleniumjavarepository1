404b82a2e2dc7e695a29b5ae65a58016b706b7a8
chandulalmr <chandhulal.mr@gmail.com>
2022-07-31 22:23:15 +0530

    Testng

---
+++ SeleniumJava/src/test/java/test/testng_Demo.java
@@ -0,0 +1,38 @@
+package test;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+
+import Pages.googlesearchpagePOM;
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class testng_Demo {
+	static WebDriver driver = null;
+
+@BeforeTest
+	public static void Setup() {
+		WebDriverManager.chromedriver().setup();
+		driver = new ChromeDriver();
+		
+	}
+
+@org.testng.annotations.Test
+	public static void Test() {
+		driver.get("https://www.google.com");
+		googlesearchpagePOM searchpageobj = new googlesearchpagePOM(driver);
+		searchpageobj.setTextInSearchbox("A B C");
+		searchpageobj.clickSearchButton();	
+		
+	}
+
+@AfterTest
+	public static void teardorn() {
+		
+		driver.close();
+		driver.quit();
+		System.out.println("Run Successfully");
+}
+	
+}
