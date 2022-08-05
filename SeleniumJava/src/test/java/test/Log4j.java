86b6b60155e9da231aa96794ca8baddf887d4fd2
chandulalmr <chandhulal.mr@gmail.com>
2022-08-06 01:48:26 +0530

    Log4j

---
+++ SeleniumJava/src/test/java/test/Log4j.java
@@ -0,0 +1,23 @@
+package test;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class Log4j {
+
+	// we need to make this static because we are referring it in static method
+	private static Logger loggerobj = LogManager.getLogger(Log4j.class);
+
+	public static void main(String[] args) {
+		System.out.println("Hi");
+
+		loggerobj.info("This is information");
+		loggerobj.error("This is a error");
+		loggerobj.warn("This is a Warn");
+		loggerobj.fatal("This is a Fatal");
+
+		System.out.println("Completed");
+	}
+
+
+}
